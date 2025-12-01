331f4ee539787e285cc4a7f6eca10890905f94fb
myk3l <ayomikesam@gmail.com>
2025-12-01 13:53:53 +0100

    install jwt

--- route/routes.js
+++ route/routes.js
@@ -6,10 +6,11 @@ import {
   usersHandler,
 } from "../API/handler.js";
 import { simpleMiddleWare } from "../middlewares/main.js";
+import auth from "../middlewares/auth.js";
 
 const route = Router();
-route.get("/user/", usersHandler);
-route.post("/add/new/poduct",   newProduct);
+route.get("/user", auth, usersHandler);
+route.post("/add/new/poduct", newProduct);
 // route.post()
 
 export default route;
